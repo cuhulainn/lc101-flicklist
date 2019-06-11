1457a5182a7fc8db890e888d1e0f912ecdae0089
Connor McGinn <connor.mcginn@gmail.com>
2019-06-10 19:38:10 -0500

    Added get_random_movie functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,22 +9,30 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
-
+    movie2 = get_random_movie()
+    while movie2 == movie:
+        movie2 = get_random_movie()
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
     content += "</ul>"
-
+    
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + movie2 + "</li>"
+    content += "</ul>"
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movie_list = ["The Big Lebowski", "Ferris Beuller's Day Off", "Wayne's World", "Stand By Me", "The Sandlot", "The Goonies", "Big", "Top Gun"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    return movie_list[random.randint(1,len(movie_list) - 1)]
 
 
 app.run()
